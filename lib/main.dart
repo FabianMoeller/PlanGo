10df66498b788e24d081843e0f529d4fb3cd082f
Adrian Sarwari <sarwariadrian@gmail.com>
2019-10-16 14:49:39 +0200

    first test if git works

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+//test if git works
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
